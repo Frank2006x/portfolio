46ecd702bf25a81b7d53e9cc15f446a276653486
Frank2006x <frank2006x@gmail.com>
2025-07-06 20:28:39 +0530

    added about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
+import About from "./sections/About";
 const App = () => {
   return (
     <div className="container mx-auto max-w-7xl">
       <Navbar />
       <Hero />
+      <About />
       <section className="min-h-screen"></section>
     </div>
   );
